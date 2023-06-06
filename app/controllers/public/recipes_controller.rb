698824e126fc59577f740e33bfac89ccf96a0f70
xNissyx <126715665+xNissyx@users.noreply.github.com>
2023-06-06 04:38:43 +0000

    レシピCRUD周りを追加

--- app/controllers/public/recipes_controller.rb
+++ app/controllers/public/recipes_controller.rb
@@ -1,2 +1,49 @@
 class Public::RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def show
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = current_user.recipes.new(recipe_params)
+    if @recipe.save
+      redirect_to recipe_path(@recipe), notice: 'レシピを作成しました'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @recipe.update(recipe_params)
+      redirect_to @recipe, notice: 'レシピを更新しました'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @recipe.destroy
+    redirect_to recipes_url, notice: 'レシピを削除しました'
+  end
+
+  private
+
+    def set_recipe
+      @recipe = Recipe.find(params[:id])
+    end
+
+    def recipe_params
+      params.require(:recipe).permit(:title, :description, :calorie, :user_id)
+    end
 end
