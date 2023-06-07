759ad48fdcb0ff426ecf1f5ee681dd5def798b86
xNissyx <126715665+xNissyx@users.noreply.github.com>
2023-06-07 00:15:53 +0000

    コメント管理機能追加

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,4 +1,11 @@
 class Admin::CommentsController < ApplicationController
   def index
+    @comments = Comment.all
+  end
+
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    redirect_to admin_comments_path
   end
 end
