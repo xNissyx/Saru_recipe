4fd22efc3e667ac178224b60071a756b3ec7fb81
xNissyx <126715665+xNissyx@users.noreply.github.com>
2023-06-19 18:09:31 +0000

    不要な記述削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
-  # validates :password, presence: true, length: { minimum: 6 }
 
   has_many :recipes, dependent: :destroy
   has_many :comments, dependent: :destroy
@@ -14,12 +11,10 @@ class User < ApplicationRecord
   has_many :bookmarking_recipes, through: :bookmarks, source: :recipe
 
   has_one_attached :image
-  # ActiveStorage用
   def get_image
     if image.attached?
       image
     else
-    # Blobオブジェクトにしかvariantメソッドは使えないため、ここで変換している
       ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join('app', 'assets', 'images', 'no_image.jpg')), filename: 'no_image.jpg')
     end
   end
