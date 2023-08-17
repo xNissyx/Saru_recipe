5cc3fa951eda361a03368be91314fc0d4ce7cf2a
xNissyx <126715665+xNissyx@users.noreply.github.com>
2023-08-17 13:58:35 +0000

    リファクタリング

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,14 +1,12 @@
 class Public::CommentsController < ApplicationController
   before_action :authenticate_user!, only: [:create]
+  before_action :set_recipe, only: [:create, :destroy]
 
   def create
-    @recipe = Recipe.find(params[:recipe_id])
     @comment = current_user.comments.new(comment_params)
     @comment.recipe_id = @recipe.id
     if @comment.save
-      respond_to do |format|
-        format.js
-      end
+      respond_to :js
     else
       flash[:notice] = "コメント内容を入力してください"
       redirect_to recipe_path(@recipe)
@@ -16,14 +14,18 @@ class Public::CommentsController < ApplicationController
   end
 
   def destroy
-    recipe = Recipe.find(params[:recipe_id])
-    comment = recipe.comments.find(params[:id])
+    comment = @recipe.comments.find(params[:id])
     comment.destroy
-    redirect_to recipe_path(recipe)
+    redirect_to recipe_path(@recipe)
   end
 
   private
-    def comment_params
-      params.require(:comment).permit(:content, :score)
-    end
+  
+  def set_recipe
+    @recipe = Recipe.find(params[:recipe_id])
+  end
+  
+  def comment_params
+    params.require(:comment).permit(:content, :score)
+  end
 end
